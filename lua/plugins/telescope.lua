f4ca0acb820a2675cedb533b64754953853ef5d2
mattia72 <mattia72@gmail.com>
2023-12-31 14:13:09 +0100

    build telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,5 +1,6 @@
 return {
   "nvim-telescope/telescope.nvim",
+  build = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build',
   enabled = true,
   keys = function()
     local Util = require("lazyvim.util")
